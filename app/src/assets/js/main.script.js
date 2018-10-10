b8be02664fbf0e01849a9f14bc49f45b788643fc
Hossein Marzban <marzban98@gmail.com>
2018-10-10 14:45:32 +0330

    releas

--- app/src/assets/js/main.script.js
+++ app/src/assets/js/main.script.js
@@ -1,39 +1,32 @@
+var app = new Ned();
 
+app.config({
+    customAttributeNavigate: "ndhref",
+    defualtRoot: '/'
+});
 
-            var app = Ned();
+app.router.add('/', {
+    name: "Index Page",
+    html: "./pages/home/home.route.page.html",
+    style: "./pages/home/home.route.style.css",
+    script: "./pages/home/home.route.script.js",
+    controller: function () { /*console.log("/home router loaded")*/ }
+});
 
-            app.config({
-                customAttributeNavigate:"ndhref",
-                defualtRoot:'/'
-            });
+app.router.add('/about', {
+    name: "About Page",
+    html: "./pages/about/about.route.page.html",
+    style: "./pages/about/about.route.style.css",
+    script: "./pages/about/about.route.script.js",
+    controller: function () { /*console.log("/about router loaded")*/ }
+});
 
-      
-            app.router.add('/',{
-                name:"Index Page",
-                html:  "./pages/home/home.route.page.html",
-                style: "./pages/home/home.route.style.css",
-                script:"./pages/home/home.route.script.js",
-                controller: function(){ /*console.log("/home router loaded")*/ }
-            });
+app.component.add('component-header', {
 
-            app.router.add('/about',{
-                name: "About Page",
-                html:  "./pages/about/about.route.page.html",
-                style: "./pages/about/about.route.style.css",
-                script:"./pages/about/about.route.script.js",
-                controller: function(){ /*console.log("/about router loaded")*/ }
-            });
+    html: "./components/header/header.component.page.html",
+    style: "./components/header/header.component.style.css",
+    script: "./components/header/header.component.script.js",
+    controller: function () { /*console.log("<component-header></component-header> component loaded")*/ }
+});
 
-            
-
-
-            app.component.add('component-header',{   
-                
-                html:  "./components/header/header.component.page.html",
-                style: "./components/header/header.component.style.css",  
-                script:"./components/header/header.component.script.js",
-                controller: function(){ /*console.log("<component-header></component-header> component loaded")*/ }  
-            });
-
-
-           app.init();
+app.init();
